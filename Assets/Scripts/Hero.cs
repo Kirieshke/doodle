294ced5f3136a274ebb3091d9c525a7533e01e53
Kirieshka <67062457+Kirieshke@users.noreply.github.com>
2021-01-15 00:37:01 +0300

    Add files via upload

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour
+{
+    private float speed = 3.0F;
+
+    private bool isGrounded = false;
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal")) 
+        {
+            Run();
+        }
+        
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        sprite.flipX = direction.x < 0.0F;
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
+
+        isGrounded = colliders.Length > 1;
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    public int numberOfPlatform = 10000;
+    
+    public float levelWidht = 0.3f;
+    public float minY = .1f;
+    public float maxY = 1.5f;
+
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for(int i = 0; i < numberOfPlatform; ++i)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x += Random.Range(-levelWidht-1, levelWidht+1);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
